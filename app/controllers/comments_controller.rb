ecd53ae3122b3409d2cb9903e138ae5aeb535ccd
marvinick <marviluw@gmail.com>
2013-10-10 23:20:23 -0500

    helper

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,17 +2,13 @@ class CommentsController < ApplicationController
 def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
-    @comment.user_id = 1
 
     if @comment.save
-      flash[:notice] = "Your comment was added"
-      redirect_to post_path(@post)
+      redirect_to post_path(@post), notice: "Your comment was added"
     else
       render 'posts/show'
     end
   end
-
-  private
 
   def comment_params
     params.require(:comment).permit(:body)
