c2f807e3e544786a72c0996b2712d46ff9fc2c29
marvinick <marviluw@gmail.com>
2013-10-13 15:25:32 -0500

    authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # 1. get the user obj
+    # 2. see if the password matches
+    # 3. if so, log in
+    # 4. if not, error message
+
+    user = User.find_by(username: params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "You have logged in!"
+    else
+      flash[:error] = "There is something wrong"
+      redirect_to login_path
+    end
 
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:notice] = "You have logged out"
+    redirect_to main_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ PostitTemplate::Application.routes.draw do
   root to: 'posts#index', as: 'main'
 
   get '/register', to: 'users#new'
-  get '/login', to: 'session#new'
-  post '/login', to: 'session#create'
-  post '/logout', to: 'session#destroy'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
 
 
   resources :posts, except: [:destroy] do
