13c6d3de9f6ae264d53d50789bbd38052e5d696b
marvinick <marviluw@gmail.com>
2013-10-17 23:00:37 -0500

    polymorphic model,db and arrows

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user, foreign_key: 'user_id', class_name: 'User'
-
+  has_many :votes, as: :voteable
   validates :body, presence: true
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
   has_many :comments
+  has_many :votes, as: :voteable
 
   validates :title, presence: true
   validates :url, presence: true
