b016ee6d1c6793a8e710cc644b67f93d87e1da00
marvinick <marviluw@gmail.com>
2013-10-19 14:10:31 -0500

    css

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,10 +2,11 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user, foreign_key: 'user_id', class_name: 'User'
   has_many :votes, as: :voteable
-  validates :body, presence: true
-  has_many :votes, as: :voteable
 
-def total_votes
+  validates :body, presence: true
+
+
+def votes_total
   self.up_votes - self.down_votes
 end
 
